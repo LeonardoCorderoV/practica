3e26345177dd907a6ff67d58068e205c606e2006
LeonardoCorderoV <151818579+LeonardoCorderoV@users.noreply.github.com>
2023-11-26 19:33:24 -0500

    Add files via upload

---
+++ SpringBootDataJPAwithMySQL/src/main/java/com/dev/springboot/dataJpa/SpringBootDataJpAwithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.dataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpAwithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpAwithMySqlApplication.class, args);
+	}
+
+}
